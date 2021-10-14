4df35e279b723743e6bc23b0c65851f5e7913761
berryllium <gorkundp@yandex.ru>
2021-10-14 23:49:13 +0300

    f

--- web/app/iskra/main/ajax.php
+++ web/app/iskra/main/ajax.php
@@ -4,14 +4,16 @@
  */
 require_once $_SERVER['DOCUMENT_ROOT']. '/db.php';
 
-$tables = ['test_iscra__cur1', 'test_iscra__pusk1', 'test_iscra__pusk2', 'test_iscra__pusk3'];
+$tables = ['test_iskra__cur1', 'test_iskra__pusk1', 'test_iskra__pusk2', 'test_iskra__pusk3'];
 
 foreach ($tables as $table) {
     $res = $db->query("SELECT * FROM $table ORDER BY ID DESC LIMIT 1");
-    $data = $res->fetch_assoc();
-    $name = str_replace('test_iscra__', '', $table);
-    $result[$name]['val'] = $data['MESSAGE'];
-    $result[$name]['date'] = $data['DATE_INSERT'];
+    if($res) {
+        $data = $res->fetch_assoc();
+        $name = str_replace('test_iskra__', '', $table);
+        $result[$name]['val'] = $data['MESSAGE'];
+        $result[$name]['date'] = $data['DATE_INSERT'];
+    }
 }
 
 die(json_encode($result, JSON_UNESCAPED_UNICODE));
--- web/app/iskra/main/script.js
+++ web/app/iskra/main/script.js
@@ -17,12 +17,16 @@ $(function (){
             dataType: 'json',
 
             success: function (data) {
-                let level = Number(data.cur1.val) * (100 / maxLevel)
-                if(level > 100) level = 100;
-                cur.height(level + '%')
-                Number(data.pusk1.val) ? pomp1.addClass('active') : pomp1.removeClass('active')
-                Number(data.pusk2.val) ? pomp2.addClass('active') : pomp2.removeClass('active')
-                Number(data.pusk3.val) ? pomp3.addClass('active') : pomp3.removeClass('active')
+                try {
+                    let level = Number(data.cur1.val) * (100 / maxLevel)
+                    if(level > 100) level = 100;
+                    cur.height(level + '%')
+                    Number(data.pusk1.val) ? pomp1.addClass('active') : pomp1.removeClass('active')
+                    Number(data.pusk2.val) ? pomp2.addClass('active') : pomp2.removeClass('active')
+                    Number(data.pusk3.val) ? pomp3.addClass('active') : pomp3.removeClass('active')
+                } catch (e) {
+                    console.log(e.name + ': ' + e.message);
+                }
                 timer()
             }
         })
